ff70baed2e82262f08000a9727daf29c178ea7fd
Danilo Lira <danilo.lira01@gmail.com>
2021-05-01 11:19:50 -0300

    feat(login): moves auth function to controller

--- src/controllers/AuthenticationController.ts
+++ src/controllers/AuthenticationController.ts
@@ -1,14 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
-import { getRepository } from 'typeorm';
+import { getRepository, getCustomRepository } from 'typeorm';
 import AppUser from '@models/AppUser';
 import jwt from 'jsonwebtoken';
+import AppUserRepository from '../repositories/AppUserRepository';
 
 class AuthenticationController {
   async tokenVerify(req, res, next){
     const token = req.headers['x-access-token'];
     
     if (!token) {
-      return res.status(401).json({ auth: false, message: 'No token provided.' });
+      return res.status(401).json({ auth: false, message: 'No token provided.'});
     }
 
     jwt.verify(token, process.env.SECRET, (err, decoded) => {
@@ -17,11 +18,43 @@ class AuthenticationController {
         return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
       } 
       
-      // se tudo estiver ok, salva no request para uso posterior
       req.headers['userId'] = decoded.id;
       next();
     });
-}
+  }
+  async signIn(req, res, next) {
+    const repository = getRepository(AppUser);
+
+    const email = req.body.email
+    const password =  req.body.password
+
+    if (!email || !password) {
+      res.locals.status = 400
+      res.locals.data = "Email ou senha não fornecidos"
+      return next()
+    }
+
+    const user = await repository.findOne({where: {email: email, password: password}})
+    
+    if (!user) {
+      res.locals.status = 400
+      res.locals.data = "Usuário ou senha não correspondentes"
+      return next()
+    }
+
+    const token = jwt.sign(user.id, process.env.SECRET)
+    
+    res.locals.status = 200
+    res.locals.data = {auth: true, token: token}
+    return next()
+
+
+  }
+  async signOut(req, res, next) {
+    const token = req.headers['x-access-token'];
+
+
+  }
 }
 
 export default new AuthenticationController();
