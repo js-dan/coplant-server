283abd645cc97b32e0ab186436c86cdff3844385
Danilo Lira <danilo.lira01@gmail.com>
2021-05-01 15:39:02 -0300

    feat(plant): Fix migration error

--- src/database/migrations/1619880698374-app_plant.ts
+++ src/database/migrations/1619880698374-app_plant.ts
@@ -8,8 +8,8 @@ export class appPlant1619880698374 implements MigrationInterface {
         ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
         "name" character varying NOT NULL,
         "imageURL" character varying,
-        "qtd" integer NOT NULL),
-        CONSTRAINT "PK_22a5c4a3d9b2fb8e4e73fc4ada1" PRIMARY KEY ("id"))
+        "qtd" integer NOT NULL,
+        CONSTRAINT "PK_22a5c4a3d9b2fb8e4e73fc4ada2" PRIMARY KEY ("id"))
         `);
     }
 
