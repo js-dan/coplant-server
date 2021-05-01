d2f40a0946af669e3bb0778e5faa2ad961b6e9a1
LeonardoL <leolimamack@gmail.com>
2021-05-01 16:13:09 -0300

    feat(order) : Fix create order

--- src/models/AppOrder.ts
+++ src/models/AppOrder.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryGeneratedColumn, Column, ManyToOne} from "typeorm"; 
+import {Entity, PrimaryGeneratedColumn, Column, ManyToOne, Double} from "typeorm"; 
 import AppUser from "./AppUser"; 
 @Entity() 
 export class AppOrder{  
@@ -8,18 +8,24 @@ export class AppOrder{
    
    @Column() 
    start_date: string;
+
    @Column()
    end_date: string;
+
    @Column()
    price: number;
+
    @Column()
-   score_caregiver: string;
+   score_caregiver: number;
+
    @Column()
-   score_client: string;
+   score_client: number;
+
    @Column()
    comment: string;
+
    @Column()
    description: string;
    
-//    @ManyToOne(type => AppUser, user => user.order) user: AppUser; 
+   @ManyToOne(type => AppUser, user => user.order) user: AppUser; 
 }
