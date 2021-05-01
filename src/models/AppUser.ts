364470e1d6996e8e0b8b2582a392fc292e939be5
Danilo Lira <danilo.lira01@gmail.com>
2021-05-01 18:46:59 -0300

    removes uuid

--- src/models/AppUser.ts
+++ src/models/AppUser.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export default class AppUser {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn()
   id!: string;
 
   @Column()
